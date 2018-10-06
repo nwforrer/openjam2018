1f6ef47891288b1a8a6114380a7c4692290fed0d
Nick Forrer <nwforrer@gmail.com>
2018-10-05 22:37:02 -0400

    Add awareness notifications to citizens.

--- Game.gd
+++ Game.gd
@@ -1,17 +1,35 @@
 extends Node
 
+signal update_awareness
+
 var envelope_scene
 var money = 0
+var awareness = 0
+
+var money_label
+var awareness_label
+var alert_label
 
 const money_label_format = "Money: %s"
+const awareness_label_format = "Citizen Awareness: %s"
 
 func _ready():
 	$Camera.target = $Player
 	envelope_scene = preload("res://player/Envelope.tscn")
+	
+	money_label = find_node("MoneyLabel")
+	awareness_label = find_node("AwarenessLabel")
+	alert_label = find_node("AlertLabel")
 	update_money()
+	update_awareness()
 	
 func update_money():
-	$Camera/GUI/MoneyLabel.text = money_label_format % money
+	if money_label:
+		money_label.text = money_label_format % money
+	
+func update_awareness():
+	if awareness_label:
+		awareness_label.text = awareness_label_format % awareness
 
 func _on_Player_spawn_envelope(pos, rot):
 	var envelope = envelope_scene.instance()
@@ -21,8 +39,24 @@ func _on_Player_spawn_envelope(pos, rot):
 	add_child(envelope)
 	
 func _on_Envelope_collision(env, collider):
-	if collider.money_held:
-		money += collider.money_held
-		update_money()
+	collider.awareness -= 1
+	if collider.awareness <= 0:
+		if collider.money_held:
+			money += collider.money_held
+			update_money()
+		collider.queue_free()
+		
 	env.queue_free()
-	collider.queue_free()
+
+func _on_awareness_timer_timeout():
+	alert_label.text = "Citizens, be aware of spam!"
+	$alert_timer.start()
+	
+	awareness += 10
+	emit_signal('update_awareness', awareness)
+	update_awareness()
+	$awareness_timer.wait_time *= 2
+	$awareness_timer.start()
+
+func _on_alert_timer_timeout():
+	alert_label.text = ""
--- citizen/Citizen.gd
+++ citizen/Citizen.gd
@@ -6,12 +6,18 @@ export (int) var money_held = 10
 
 var state
 var speed = 100
+var awareness = 1
+var total_awareness = 1
 var velocity = Vector2()
 
 func _ready():
 	change_state(IDLE)
+	
+	get_parent().connect('update_awareness', self, '_on_Update_awareness')
 
 func _process(delta):
+	$ProgressBar.value = awareness
+	$ProgressBar.max_value = total_awareness
 	match state:
 		IDLE:
 			pass
@@ -19,6 +25,7 @@ func _process(delta):
 			pass
 
 func _physics_process(delta):
+	$ProgressBar.set_rotation(-rotation)
 	move_and_slide(velocity)
 
 func change_state(new_state):
@@ -39,3 +46,8 @@ func _on_change_state_timer_timeout():
 		change_state(WALKING)
 	elif state == WALKING:
 		change_state(IDLE)
+		
+func _on_Update_awareness(new_awareness):
+	var delta = new_awareness - total_awareness
+	total_awareness = new_awareness
+	awareness += delta
